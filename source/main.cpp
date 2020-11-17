83e07c5fa7e96642f1e432d974dc2494edcc26c0
Ivan <suprun.ivan300@gmail.com>
2020-11-17 11:39:50 +0200

    OpenGL and GLFW initialization

--- source/main.cpp
+++ source/main.cpp
@@ -1,25 +1,43 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 
 using namespace std;
+
+void glfwWindowSizeCallback(GLFWwindow*, int, int);
+void glfwKeyCallback(GLFWwindow*, int, int, int, int);
+
+int windowSizeX = 640;
+int windowSizeY = 480;
+
+
 
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        cout << "glfw failed" << endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(windowSizeX, windowSizeY, "Education Game", nullptr, nullptr);
     if (!window)
     {
+        cout << "Window failed" << endl;
         glfwTerminate();
         return -1;
     }
 
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
@@ -29,7 +47,8 @@ int main(void)
         return -1;
     }
 
-    cout << "OpenGL" << GLVersion.major << "." << GLVersion.minor << endl;
+    cout << "Renderer:" << glGetString(GL_RENDERER)<<endl;
+    cout << "OpenGL version:" << glGetString(GL_VERSION) << endl;
 
     glClearColor(0, 1, 0, 1);
 
@@ -48,4 +67,16 @@ int main(void)
 
     glfwTerminate();
     return 0;
+}
+
+void glfwWindowSizeCallback(GLFWwindow* window, int width, int height) {
+    windowSizeX = width;
+    windowSizeY = height;
+    glViewport(0,0, windowSizeX, windowSizeY);
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
 }
